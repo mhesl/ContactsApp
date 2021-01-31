e0675eec39bbcfdc90720fd2c9bbc862ceb42301
mohamadeslami <mohammadeslami1379@yahoo.com>
2021-01-31 17:15:00 +0330

    Splash activity and login activity implemented

--- app/src/main/java/com/example/contactsapp/splash/LoginActivity.kt
+++ app/src/main/java/com/example/contactsapp/splash/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.contactsapp.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.contactsapp.MainActivity
 import com.example.contactsapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/contactsapp/splash/SplashActivity.kt
+++ app/src/main/java/com/example/contactsapp/splash/SplashActivity.kt
@@ -1,12 +1,32 @@
 package com.example.contactsapp.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.contactsapp.MainActivity
 import com.example.contactsapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        val firstStart = prefs.getBoolean("firstStart", true)
+        Handler(Looper.myLooper()!!).postDelayed({
+            val nextIntent = if (firstStart) {
+                // move to start page because this the first time login
+                val editor = prefs.edit()
+                editor.putBoolean("firstStart", false)
+                editor.apply()
+                Intent(this@SplashActivity, LoginActivity::class.java)
+            } else {
+                // it's not the first time login
+                Intent(this@SplashActivity, MainActivity::class.java)
+            }
+            startActivity(nextIntent)
+            finish()
+        }, 2000)
     }
 }
