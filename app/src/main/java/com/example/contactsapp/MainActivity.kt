d84f03676b26f803c5fb2aca62327e50370484a6
mohamadeslami <mohammadeslami1379@yahoo.com>
2021-02-02 23:04:33 +0330

    some bugs fixed

--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.contactsapp
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
